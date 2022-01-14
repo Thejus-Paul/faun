e87b4eeecbe16d8396daa305b435b415459c3521
Thejus Paul <thejuspaul@pm.me>
2022-01-14 22:40:51 +0530

    Added audio record feature

--- popup/index.js
+++ popup/index.js
@@ -5,13 +5,17 @@ recorderElement.addEventListener('click', async () => {
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: setPageBackgroundColor
+    function: recorder
   })
 })
 
-async function setPageBackgroundColor () {
-  const configuration = { audio: true, video: { width: 1280, height: 720 } }
+async function recorder () {
+  const configuration = { audio: false, video: { width: 1280, height: 720 } }
   const stream = await navigator.mediaDevices.getDisplayMedia(configuration)
+  const audio = await navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: false
+  })
 
   // needed for better browser support
   const mime = MediaRecorder.isTypeSupported('video/webm; codecs=vp9')
@@ -19,13 +23,20 @@ async function setPageBackgroundColor () {
     : 'video/webm'
 
   const mediaRecorder = new MediaRecorder(stream, { mimeType: mime })
+  const audioRecorder = new MediaRecorder(audio, { mimeType: 'audio/webm' })
 
   const frames = []
   mediaRecorder.addEventListener('dataavailable', ({ data }) =>
     frames.push(data)
   )
 
+  const tracks = []
+  audioRecorder.addEventListener('dataavailable', ({ data }) =>
+    tracks.push(data)
+  )
+
   mediaRecorder.addEventListener('stop', () => {
+    audioRecorder.stop()
     const blob = new Blob(frames, { type: frames[0].type })
     const url = URL.createObjectURL(blob)
 
@@ -35,5 +46,16 @@ async function setPageBackgroundColor () {
     a.click()
   })
 
+  audioRecorder.addEventListener('stop', () => {
+    const blob = new Blob(tracks, { type: tracks[0].type })
+    const url = URL.createObjectURL(blob)
+
+    const a = document.createElement('a')
+    a.href = url
+    a.download = 'audio.wav'
+    a.click()
+  })
+
   mediaRecorder.start()
+  audioRecorder.start()
 }
